9b5c9289150735a5a62c6d492411e1914371c711
UMESH KUMAR <246810umesh@gmail.com>
2026-02-16 20:39:23 +0530

    add Navbar and SideMenu components, update Dashboard

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -1,15 +1,16 @@
-import React, { useContext } from 'react'
+// import React, { useContext } from 'react'
 import { useUserAuth } from '../../hooks/useUserAuth'
 import { UserContext } from '../../context/userContext';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
 
 const Dashboard = () => {
   useUserAuth();
 
-  const { user } = useContext(UserContext);
+  // const { user } = useContext(UserContext);
 
 
   return (
-    <DashboardLayout>Dashboard
+    <DashboardLayout activeMenu="Dashboard">Dashboard
     </DashboardLayout>
   )
 }
