4e3ee43b92fae71e1937591db9f9a63cf7d3f145
UMESH KUMAR <246810umesh@gmail.com>
2026-02-21 18:58:35 +0530

    add Modal for SelectUsers

--- frontend/Task-Manager/src/components/Inputs/SelectUsers.jsx
+++ frontend/Task-Manager/src/components/Inputs/SelectUsers.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import { LuUser, LuUsers } from 'react-icons/lu';
+import Modal from '../Modal';
+import AvatarGroup from '../AvatarGroup';
 
 const SelectUsers = ({ selectedUsers = [], setSelectUsers }) => {
   const [allUsers, setAllUsers] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(true);
   const [tempSelectedUsers, setTempSelectedUsers] = useState([]);
 
   const getAllUsers = async () => {
@@ -36,24 +38,76 @@ const SelectUsers = ({ selectedUsers = [], setSelectUsers }) => {
     .filter((user) => selectedUsers.includes(user._id))
     .map((user) => user.profileImageUrl);
 
-useEffect(() => {
-  getAllUsers();
-}, []);
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
-useEffect(() => {
-  if(selectedUsers && selectedUsers.length === 0) {
-    setTempSelectedUsers([]);
-  }
-}, [selectedUsers]);
+  useEffect(() => {
+    if (selectedUsers && selectedUsers.length === 0) {
+      setTempSelectedUsers([]);
+    }
+  }, [selectedUsers]);
 
-return <div className='space-y-4 mt-2'>
-    {selectedUserAvatars.length === 0 &&(
+  return (
+    <div className='space-y-4 mt-2'>
+      {selectedUserAvatars.length === 0 && (
         <button className='card-btn' onClick={() => setIsModalOpen(true)}>
-            <LuUsers className='text-sm'/> Add Members
+          <LuUsers className='text-sm' /> Add Members
         </button>
-    )}
-</div>;
+      )}
 
+      {selectedUserAvatars.length > 0 &&(
+        <div className='cursor-pointer' onClick={() => setIsModalOpen(true)}>
+          <AvatarGroup avatars={selectedUserAvatars} maxVisible={3}/>
+        </div>
+      )}
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Select Users"
+      >
+        <div className='space-y-4 h-[60vh] overflow-y-auto'>
+          {allUsers.map((user) => (
+            <div
+              key={user._id}
+              className='flex items-center gap-4 p-3 border-b border-grey-200'
+            >
+              <img
+                src={user.profileImageUrl}
+                alt={user.name}
+                className='w-10 h-10 rounded-full'
+              />
+              <div className='flex-1'>
+                <p className='font-medium text-grey-800 dark:text-white'>
+                  {user.name}
+                </p>
+                <p className='text-[13px] text-grey-500'>
+                  {user.email}
+                </p>
+              </div>
+
+              <input
+                type='checkbox'
+                checked={tempSelectedUsers.includes(user._id)}
+                onChange={() => toggleUserSelection(user._id)}
+                className='w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none'
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className='flex justify-end gap-4 pt-4'>
+          <button className='card-btn' onClick={()=> setIsModalOpen(false)}>
+            CANCEL
+          </button>
+          <button className='card-btn-fill' onClick={handleAssign}>
+            DONE
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default SelectUsers;
