ff4bd28cd98d4950fd734c8a4e0e6e82856ed59d
UMESH KUMAR <246810umesh@gmail.com>
2026-02-26 21:57:39 +0530

    add managetasks api and download button

--- frontend/Task-Manager/src/components/layouts/SideMenu.jsx
+++ frontend/Task-Manager/src/components/layouts/SideMenu.jsx
@@ -59,11 +59,11 @@ const SideMenu = ({ activeMenu }) => {
     {sideMenuData.map((item, index) => (
       <button
         key={`menu_${index}`}
-        className={`w-full flex items-center gap-4 text-[15px] ${
-          activeMenu === item.path
+        className={`w-full flex items-center gap-4 text-[15px] py-3 px-6 mb-3 cursor-pointer
+          ${activeMenu == item.label
             ? "text-primary bg-linear-to-r from-blue-50/40 to-blue-100/50 border-r-3"
             : ""
-        } py-3 px-6 mb-3 cursor-pointer`}
+          }`}
         onClick={() => handleClick(item.path)}
       >
         <item.icon className="text-xl " />
--- frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
+++ frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
@@ -1,8 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
+import { useNavigate } from 'react-router-dom'
+import { LuFileSpreadsheet } from 'react-icons/lu'
+
 
 const ManageTasks = () => {
+
+  const [allTasks, setAllTasks] = useState([]);
+
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("All");
+
+  const navigate = useNavigate();
+
+  const getAllTasks = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {
+          status: filterStatus === "All" ? "" : filterStatus,
+        },
+      });
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+      // Map statusSummary data with fixed labels and order
+      const statusSummary = response.data?.statusSummary || {};
+
+      const statusArray = [
+        { label: "All", count: statusSummary.all || 0 },
+        { label: "Pending", count: statusSummary.pendingTasks || 0 },
+        { label: "In Progress", count: statusSummary.inProgressTasks || 0 },
+        { label: "Completed", count: statusSummary.completedTasks || 0 },
+      ];
+
+      setTabs(statusArray);
+    } catch (error) {
+      console.error("Error fetching tasks:", error.response?.data || error.message);
+    }
+  };
+
+  const handleClick = (taskData) => {
+    navigate('/admin/create-task', { state: { taskId: taskData.id } });
+  }
+
+  // download task report
+  const handleDownloadReport = async (taskId) => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.DOWNLOAD_REPORT, {
+        params: { taskId },
+        responseType: 'blob',
+      });
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', `task-report-${taskId}.pdf`);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading task report:", error.response?.data || error.message);
+    }
+  };
+
+  useEffect(() => {
+    getAllTasks(filterStatus);
+    return () => { };
+  }, [filterStatus]);
+
   return (
-    <div>ManageTasks</div>
+    <DashboardLayout activeMenu="Manage Tasks">
+      <div className='mt-5'>
+        <div className='flex flex-col md:flex-row md:items-center justify-between'>
+          <div className='flex items-center justify-between gap-3'>
+            <h2 className='text-xl md:text-xl font-medium'>
+              My Tasks
+            </h2>
+
+            <button
+              className='flex download-btn'
+              onClick={handleDownloadReport}
+            >
+              <LuFileSpreadsheet className="text-lg" />
+              Download Report
+            </button>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
