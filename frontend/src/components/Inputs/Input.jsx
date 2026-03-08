f69fb7699719267d1a6ef089eec9aa33e6246605
UMESH KUMAR <246810umesh@gmail.com>
2026-03-08 11:23:22 +0530

    add readme

--- frontend/src/components/Inputs/Input.jsx
+++ frontend/src/components/Inputs/Input.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6'
 
 const Input = ({ value, onChange, label, placeholder, type }) => {
   const [showPassword, setShowPassword] = useState(false);
@@ -17,6 +18,24 @@ const Input = ({ value, onChange, label, placeholder, type }) => {
           placeholder={placeholder}
           className="w-full bg-transparent outline-none"
         />
+
+        {type === "password" && (
+          <>
+            {showPassword ? (
+              <FaRegEye
+                size={22}
+                className="text-primary cursor-pointer"
+                onClick={() => toggleShowPassword()}
+              />
+            ) : (
+              <FaRegEyeSlash
+                size={22}
+                className="text-slate-400 cursor-pointer"
+                onClick={() => toggleShowPassword()}
+              />
+            )}
+          </>
+        )}
       </div>
     </div>
   )
