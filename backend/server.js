f3fe5c981b7ddb78f40e670dcf941f50d4e65095
UMESH KUMAR <246810umesh@gmail.com>
2026-05-13 18:11:29 +0530

    fix mongo db dns connection issue

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns').promises;
+dns.setServers(["8.8.8.8","1.1.1.1"]);
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
